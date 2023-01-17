867bc08c1a67ca7b032513f9ed32f24c67488d60
gaellafond <g.lafond@aims.gov.au>
2023-01-17 16:03:43 +0800

    Version 2.8.1 - Fixed __LOCK_OLD_EREEFS_METADATA_IDS__

--- src/main/java/au/gov/aims/ncanimate/NcAnimateMetadataIdFixer.java
+++ src/main/java/au/gov/aims/ncanimate/NcAnimateMetadataIdFixer.java
@@ -126,33 +126,35 @@ public class NcAnimateMetadataIdFixer {
         Map<String, Integer> skippedMetadataCountMap = new HashMap<>();
 
         for (JSONObject metadata : metadatas) {
-            String origId = metadata.optString("_id", null);
-            if (origId != null) {
-                String idPrefix = origId.split("/")[0];
-                String fixedId = AbstractBean.safeIdValue(origId);
-                if (!origId.equals(fixedId)) {
-                    if (origId.startsWith("downloads__ereefs__")) {
-                        LOGGER.info(String.format("Lock down old metadata ID: %s", origId));
-                        incrementCount(lockedMetadataCountMap, idPrefix);
-                        metadata.put("lastModified", "3000-01-01T00:00:00.000Z");
-                        if (!DRY_RUN) {
-                            metadataManager.save(metadata, false);
-                        }
-
-                        boolean exists = metadataManager.exists(fixedId);
-                        if (exists) {
-                            LOGGER.info(String.format("Duplicate metadata ID found: %s %s", origId, fixedId));
-                            incrementCount(duplicatedIdCountMap, idPrefix);
-
-                            LOGGER.info(String.format("Deleting NEW duplicated metadata ID: %s", fixedId));
+            if (metadata != null) {
+                String origId = metadata.optString("_id", null);
+                if (origId != null) {
+                    String idPrefix = origId.split("/")[0];
+                    String fixedId = AbstractBean.safeIdValue(origId);
+                    if (!origId.equals(fixedId)) {
+                        if (origId.startsWith("downloads__ereefs__")) {
+                            LOGGER.info(String.format("Lock down old metadata ID: %s", origId));
+                            incrementCount(lockedMetadataCountMap, idPrefix);
+                            metadata.put("lastModified", "3000-01-01T00:00:00.000Z");
                             if (!DRY_RUN) {
-                                // Delete
-                                metadataManager.delete(fixedId);
+                                metadataManager.save(metadata, false);
                             }
+
+                            boolean exists = metadataManager.exists(fixedId);
+                            if (exists) {
+                                LOGGER.info(String.format("Duplicate metadata ID found: %s %s", origId, fixedId));
+                                incrementCount(duplicatedIdCountMap, idPrefix);
+
+                                LOGGER.info(String.format("Deleting NEW duplicated metadata ID: %s", fixedId));
+                                if (!DRY_RUN) {
+                                    // Delete
+                                    metadataManager.delete(fixedId);
+                                }
+                            }
+                        } else {
+                            LOGGER.info(String.format("NOT A EREEFS DOWNLOAD - SKIPPING: %s", origId));
+                            incrementCount(skippedMetadataCountMap, idPrefix);
                         }
-                    } else {
-                        LOGGER.info(String.format("NOT A EREEFS DOWNLOAD - SKIPPING: %s", origId));
-                        incrementCount(skippedMetadataCountMap, idPrefix);
                     }
                 }
             }
